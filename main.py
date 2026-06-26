2ce9681d071a3eeab6afc3672481456a34573fe9
danilamozh-maker <danilamozh@gmail.com>
2026-06-26 10:57:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -337,3 +337,4 @@ async def package_files(request: Request, files: list[UploadFile] = File(...), a
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+    
